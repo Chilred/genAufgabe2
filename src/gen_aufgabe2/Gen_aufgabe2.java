d32305732a6e494dbe5f8ff117a40e975f33ca76
Martin Wunderlich <martin.wunderlich@iem.thm.de>
2016-01-16 00:30:42 +0100

    clean up main

--- src/gen_aufgabe2/Gen_aufgabe2.java
+++ src/gen_aufgabe2/Gen_aufgabe2.java
@@ -18,29 +18,30 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Gen_aufgabe2 {
 
     static String path = "src/map/";
-//    static String filename = "05-map-10x10-36border.txt";
-    static String filename = "06-map-100x100-50.txt";
-    
+    static String filename = "05-map-10x10-36border.txt";
+//    static String filename = "06-map-100x100-50.txt";
+
     static int gencnt = 100;
     static int genlen = 36;
 
-    static double pc = 0.9;
+    static double pc = 0.0;
     static double pcEnd = 0.9;
     static double pcStep = 0.05;
-    static double pm = 0.005; // 0
+    static double pm = 0.000; // 0
     static double pmEnd = 0.2;
     static double pmStep = 0.005;
     static int max_generation = 2000; //2000
-    static boolean protection = true;
+    static boolean protection = false;
     static int maxRun = 50; //50
     static double fitMax = 36;
-//    static String replication = "NONE";
-    static String replication = "replicate50Best";
+    static String replication = "NONE";
+//    static String replication = "replicate50Best";
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
+//        long startTime = System.currentTimeMillis();
         Map map = new Map(path, filename);
 //        Gene gene1 = new Gene(36, map);
 //        gene1.fillFitness();
@@ -79,18 +80,17 @@ public class Gen_aufgabe2 {
 //        Gene g = new Gene(genlen, map);
 //        Part1
 //        startFirstRun(map);
-//        startFirstRunAll(map);
+        startFirstRunAll(map);
 //        Part2
 //        Time calcute
-        double bestPc = pc;
-        double bestPm = pm;
-        
-       approximationChecker(map, bestPc, bestPm);
-        
-        
-        long endTime = System.currentTimeMillis();
-        long totalTime = endTime - startTime;
-        System.out.println("totalTime: " + totalTime + " ms");
+//        double bestPc = pc;
+//        double bestPm = pm;
+//        
+//       approximationChecker(map, bestPc, bestPm);
+
+//        long endTime = System.currentTimeMillis();
+//        long totalTime = endTime - startTime;
+//        System.out.println("totalTime: " + totalTime + " ms");
     }
 
     public static void startFirstRun(Map map) {
@@ -101,6 +101,7 @@ public class Gen_aufgabe2 {
     }
 
     public static void startFirstRunAll(Map map) {
+        long startTime = System.currentTimeMillis();
         String result = "";
         for (double i = pc; i < pcEnd; i += pcStep) {
             for (double j = pm; j < pmEnd; j += pmStep) {
@@ -111,19 +112,29 @@ public class Gen_aufgabe2 {
             result = result + "\r\n";
         }
 //        System.out.println(result);
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        result = result + "\r\n" + "# Gesamtzeit" + totalTime + " ms";
         writeInFile(result, "FirstRun.txt");
     }
 
-    public static void approximationChecker(Map map, double pc, double pm){
+    public static void approximationChecker(Map map, double pc, double pm) {
+        long startTime = System.currentTimeMillis();
+
         RunApproximate ra = new RunApproximate(maxRun, pc, pm, gencnt, max_generation, protection, map, replication);
         String result = "";
-         for (int i = 0; i < maxRun; i++) {
+        for (int i = 0; i < maxRun; i++) {
             ra.distanceApproximated();
             result = ra.getBestFitness() + "\r\n";
         }
-         System.out.println(result);
+        System.out.println(result);
+
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        result = result + "\r\n" + "# Gesamtzeit" + totalTime + " ms";
+        writeInFile(result, "approximationChecker.txt");
     }
-    
+
     public static double getRandomDouble() {
         return ThreadLocalRandom.current().nextDouble(1);
     }
@@ -131,7 +142,7 @@ public class Gen_aufgabe2 {
     public static int getRandomInt(int value) {
         return ThreadLocalRandom.current().nextInt(value);
     }
-    
+
     public static void writeInFile(String result, String filename) {
         try {
             String content = result;
